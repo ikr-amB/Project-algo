92bfb9ba296a3a6393a670cd87883efa138e6138
naw-abid <ikramboutin2007@gmail.com>
2025-11-26 21:55:37 +0100

    Delete wordleGame.h

--- wordleGame.h
+++
@@ -1,9 +0,0 @@
-#ifndef WORDLEGAME_H_INCLUDED
-#define WORDLEGAME_H_INCLUDED
-
-#define WORD_LEN 6
-
-int read_dictoinary(char words[][WORD_LEN], const char *filename);
-void feedback(char guess[], char word[]);
-
-#endif
